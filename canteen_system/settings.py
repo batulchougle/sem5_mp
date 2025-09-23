fd33bf05cc72977433c7e1801a2200f4792fd889
batulchougle <batulchougle@gmail.com>
2025-09-24 00:31:26 +0530

    Update settings.py

--- canteen_system/settings.py
+++ canteen_system/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'users',
 ]
 
@@ -147,4 +148,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
