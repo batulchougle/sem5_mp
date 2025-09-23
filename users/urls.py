f9955f658c2725e421fc01e5d53eeeeb73b802b4
batulchougle <batulchougle@gmail.com>
2025-09-23 22:16:59 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+rom django.urls import path, include
 from django.conf import settings
-from .views import RegisterUserView, LoginUserView, CollegeListView
+from .views import RegisterUserView, LoginUserView, CollegeListView, ShopCreateView, ShopsListView, MenuCreateView,MenuListView
+from django.conf.urls.static import static
 
 
 
@@ -8,5 +9,14 @@ urlpatterns = [
     
     path('signup/', RegisterUserView.as_view(), name='signup'),
     path('login/',LoginUserView.as_view(), name='login'),
-     path('colleges/', CollegeListView.as_view(), name='college-list'),
+    path('colleges/', CollegeListView.as_view(), name='college-list'),
+    path('shops/', ShopCreateView.as_view(), name='shops'),
+    path('shops-display/', ShopsListView.as_view(), name='shops-list'),
+    path('menu/', MenuCreateView.as_view(), name='menu'),
+    path('menu-display/', MenuListView.as_view(), name='menu-list'),
+    
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
