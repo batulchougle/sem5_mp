aa2a08cceefb231bfb2e62e410f1a03fe8b8d4b8
batulchougle <batulchougle@gmail.com>
2025-09-23 22:11:30 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -50,4 +50,22 @@ class College(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_colleges',null=True, blank=True)
 
     def __str__(self):
+
         return self.name
+class Shops(models.Model):
+    id = models.BigAutoField(primary_key=True, editable=False) 
+    college = models.ForeignKey('College', on_delete=models.CASCADE, null=True, blank=True)
+    name=models.CharField(max_length=255, verbose_name=_("Name"))
+    image=models.ImageField(upload_to='shops_images/',null=True,blank=True)
+    def __str__(self):
+        return (self.name)    
+
+class Menu(models.Model):
+    id = models.BigAutoField(primary_key=True, editable=False) 
+    shop = models.ForeignKey('Shops', on_delete=models.CASCADE, null=True, blank=True)
+    name=models.CharField(max_length=255, verbose_name=_("Name"))
+    image=models.ImageField(upload_to='menu_images/',null=True,blank=True)
+    availability = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    def __str__(self):
+        return (self.name)        
